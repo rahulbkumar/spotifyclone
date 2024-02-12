694c48b9ec3f7eed5449ef66361ebc32f584229c
Rahul Kumar <85476994+rahulbkumar@users.noreply.github.com>
2024-02-12 13:23:37 -0500

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -49,8 +49,8 @@ module.exports = {
 			},
 			borderRadius: {
 				lg: "var(--radius)",
-				md: "calc(var(--radius) - 3px)",
-				sm: "calc(var(--radius) - 5px)",
+				md: "calc(var(--radius) - 2px)",
+				sm: "calc(var(--radius) - 4px)",
 			},
 			keyframes: {
 				"accordion-down": {
